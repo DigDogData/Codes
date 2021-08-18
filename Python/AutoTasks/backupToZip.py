bc8913daae267c11baf7039cbe4b73d02c87d19c
DigDogData <roy@manojitroy.com>
2021-08-17 21:15:22 -0400

    Changes made on Tue 17 Aug 2021 09:15:22 PM EDT

--- Python/AutoTasks/backupToZip.py
+++ Python/AutoTasks/backupToZip.py
@@ -5,12 +5,42 @@
 import os
 import zipfile  # module to zip/unzip files
 from pathlib import Path
+from datetime import datetime
+
+
+def main():
+    folder = Path.home() / "Documents/Scripts"  # folder to be archived
+    backupToZip(folder)
 
 
 def backupToZip(folder):
-    folder = os.path.abspath(folder)  # make sure folder is absolute
 
-    # figure out the filename this code should use
+    # attach current datetime to archive name
+    now = datetime.now()
+    dt = now.strftime("%Y-%m-%d_%H:%M:%S")
+    root = folder.name  # root path of folder
+    zipFilename = root + "_" + dt + ".zip"
+
+    # create zipped file
+    print(f"Creating '{zipFilename}'...")
+    backupZip = zipfile.ZipFile(zipFilename, "w")
+
+    # walk folder tree ('arcname' argument avoids archiving absolute path)
+    for foldername, subfolders, filenames in os.walk(folder):
+        # print(f"Adding files in {foldername}...")
+        parentPath = os.path.relpath(foldername, folder)
+        arcname = os.path.join(root, parentPath)  # archive name
+        backupZip.write(foldername, arcname)
+        for filename in filenames:
+            filePath = os.path.join(foldername, filename)
+            parentPath = os.path.relpath(filePath, folder)
+            arcname = os.path.join(root, parentPath)
+            backupZip.write(filePath, arcname)
+
+    backupZip.close()
+
+    print("Done.")
 
 
-p = Path.cwd()
+if __name__ == "__main__":
+    main()
