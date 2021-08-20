b6186fd6ae09c78cc03553785924245df012d222
DigDogData <roy@manojitroy.com>
2021-08-20 16:16:56 -0400

    Changes made on Fri 20 Aug 2021 04:16:56 PM EDT

--- Python/AutoTasks/miscDebugCodes.py
+++ Python/AutoTasks/miscDebugCodes.py
@@ -2,12 +2,21 @@
 
 # miscDebugCodes.py - Example Codes to debug codes
 
-# raise excetion
-tmp1 = 5
-tmp2 = 1
-if tmp1 != 1:
-    raise Exception("tmp1 must equal 1.")  # rest of the code does not execute
-if tmp2 == 1:
-    raise Exception("tmp2 must not equal 1.")
+# raise exception (can use inside try-except block)
+# tmp1 = 5
+# tmp2 = 1
+# if tmp1 != 1:
+#    raise Exception("tmp1 must equal 1.")  # rest of the code does not execute
+# if tmp2 == 1:
+#    raise Exception("tmp2 must not equal 1.")
 
-# assertion
+# assertion (sanity check) (do not use inside try-except block)
+# asssertion is programmer error (not user error), to be used only when
+# program is under development, and  must be allowed to fail if incorrect
+# user should never see it - using inside try-except block allows user to
+# turn it off
+ages = [26, 57, 92, 54, 22, 15, 17, 80, 47, 73]
+ages.sort()
+assert ages[0] <= ages[-1]  # assert 1st age <= last age (does nothing)
+ages.reverse()
+assert ages[0] <= ages[-1]  # assert 1st age <= last age (crashes)
