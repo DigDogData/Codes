0160a8829694ef021d03f6dc72b2db6d87f55168
DigDogData <roy@manojitroy.com>
2021-08-22 14:14:16 -0400

    Changes made on Sun 22 Aug 2021 02:14:16 PM EDT

--- Python/AutoTasks/miscDebugCodes.py
+++ Python/AutoTasks/miscDebugCodes.py
@@ -10,13 +10,38 @@
 # if tmp2 == 1:
 #    raise Exception("tmp2 must not equal 1.")
 
-# assertion (sanity check) (do not use inside try-except block)
-# asssertion is programmer error (not user error), to be used only when
-# program is under development, and  must be allowed to fail if incorrect
+# assertion (sanity check) (do not use inside try-except block);
+# asssertion is programmer error (not user error) check, to be used only when
+# program is under development, and  must be allowed to fail if incorrect;
 # user should never see it - using inside try-except block allows user to
-# turn it off
-ages = [26, 57, 92, 54, 22, 15, 17, 80, 47, 73]
-ages.sort()
-assert ages[0] <= ages[-1]  # assert 1st age <= last age (does nothing)
-ages.reverse()
-assert ages[0] <= ages[-1]  # assert 1st age <= last age (crashes)
+# turn it off;
+# running <python -0 miscDebugCodes.py> skips assert statements
+# ages = [26, 57, 92, 54, 22, 15, 17, 80, 47, 73]
+# ages.sort()
+# assert ages[0] <= ages[-1]  # assert 1st age <= last age (correct, does nothing)
+# ages.reverse()
+# assert ages[0] <= ages[-1]  # assert 1st age <= last age (incorrect, crashes)
+
+# logging (to display log messages as program runs)
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format=" %(asctime)s -  %(levelname)s -  %(message)s"
+)
+logging.debug("Start of program")
+
+
+# function to compute factorial
+def factorial(n):
+    logging.debug("Start of factorial(%s%%)" % (n))
+    total = 1
+    for i in range(n + 1):
+        total *= i
+        logging.debug("i is " + str(i) + ", total is " + str(total))
+    logging.debug("End of factorial(%s%%)" % (n))
+    return total
+
+
+print(factorial(5))
+logging.debug("End of program")
