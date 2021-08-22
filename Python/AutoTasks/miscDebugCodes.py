081908f3f943d03e19ae12e5de48bc1f72b690a9
DigDogData <roy@manojitroy.com>
2021-08-22 15:25:35 -0400

    Changes made on Sun 22 Aug 2021 03:25:35 PM EDT

--- Python/AutoTasks/miscDebugCodes.py
+++ Python/AutoTasks/miscDebugCodes.py
@@ -22,12 +22,15 @@
 # ages.reverse()
 # assert ages[0] <= ages[-1]  # assert 1st age <= last age (incorrect, crashes)
 
-# logging (to display log messages as program runs)
-
+# logging (to display log messages as program runs):
+# log messages are intended for programmer, not user (use print for user messages)
 import logging
 
+logging.disable(logging.CRITICAL)  # comment/uncomment to enable/disable all logs
 logging.basicConfig(
-    level=logging.DEBUG, format=" %(asctime)s -  %(levelname)s -  %(message)s"
+    # filename="myLog.txt",  # stores logs in a file
+    level=logging.DEBUG,  # DEBUG is lowest log level, logs everything
+    format="%(asctime)s - %(levelname)s - %(message)s",
 )
 logging.debug("Start of program")
 
@@ -36,12 +39,20 @@ logging.debug("Start of program")
 def factorial(n):
     logging.debug("Start of factorial(%s%%)" % (n))
     total = 1
-    for i in range(n + 1):
+    # for i in range(n + 1):    # uncomment to introduce error
+    for i in range(1, n + 1):
         total *= i
         logging.debug("i is " + str(i) + ", total is " + str(total))
     logging.debug("End of factorial(%s%%)" % (n))
     return total
 
 
-print(factorial(5))
+# print(factorial(5))
 logging.debug("End of program")
+
+# various logging levels (DEBUG/INFO/WARNING/ERROR/CRITICAL)
+logging.debug("Some debugging details.")
+logging.info("The logging module is working.")
+logging.warning("An error message is about to be logged.")
+logging.error("An error has occurred.")
+logging.critical("The program is unable to recover!")
