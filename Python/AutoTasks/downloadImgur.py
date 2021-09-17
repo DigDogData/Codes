fa7bd7f15b26169a44ff346025aa8c41711521ca
DigDogData <roy@manojitroy.com>
2021-09-17 06:40:26 -0400

    Changes made on Fri 17 Sep 2021 06:40:26 AM EDT

--- Python/AutoTasks/downloadImgur.py
+++ Python/AutoTasks/downloadImgur.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 # downloadImgur.py - Downloads every single Imgur images based on tag/category
+# Usage: py downloadImgur.py <category> <limit>
 
 import sys
 import os
 import bs4
 import requests
+import logging
 
 # import user-created library 'Libs'
 sys.path.append("/home/roy/Documents/DS/Codes/Python/Libs")
@@ -14,11 +16,20 @@ from util import raiseStatus  # noqa E402
 
 def main():
     os.makedirs("imgur", exist_ok=True)  # create ./imgur directory
-    tag = "coronavirus"
-    downloadImgur(tag)
+    downloadImgur()
 
 
-def downloadImgur(tag):
+def downloadImgur():
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    logging.disable(logging.CRITICAL)  # comment to enable all logging
+
+    if len(sys.argv) == 3:
+        logging.info("Command line arguments = 3")
+        category = sys.argv[1]
+        limit = sys.argv[2]
+
     url = "https://imgur.com"
 
     # download main page
